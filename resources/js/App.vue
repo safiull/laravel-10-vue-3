e5870cc7ec8117a1bf4e88d23ac3eef28778c94a
Safiull Alam <safiullalam9931@gmail.com>
2023-06-05 09:58:11 +0600

    Vue & Vite setup.

---
+++ resources/js/App.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Hello world 111.</h1>
+</template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import './bootstrap';
+import { createApp } from 'vue'
+import App from './App.vue'
 
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const app = createApp(App)
+app.mount('#app')
