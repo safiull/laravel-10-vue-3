d2eeb922b3e6d811a1b43dcbd699cbb2c1da5d98
safiull <safiullalam9931@gmail.com>
2023-06-09 00:36:53 +0600

    Seeder update succ.

--- database/seeders/DivisionSeeder.php
+++ database/seeders/DivisionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Division;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class DivisionSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        Division::factory()->count(10)->create();
     }
 }
