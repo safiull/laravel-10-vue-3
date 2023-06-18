a86f2bfe26aad70b91137596bf4eee5a68660bfc
safiull <safiullalam9931@gmail.com>
2023-06-18 23:45:47 +0600

    Middleware implemented.

--- resources/js/vue/routes.js
+++ resources/js/vue/routes.js
@@ -1,29 +1,34 @@
-import {
-    createRouter,
-    createWebHistory
-} from "vue-router";
-
-import authLayout from './components/layouts/authLayout.vue'
-import adminLayout from './components/layouts/adminLayout.vue'
-
-import Login from './components/auth/login.vue'
-import NotFound from './components/layouts/notFound.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import authLayout from "./components/layouts/authLayout.vue";
+import adminLayout from "./components/layouts/adminLayout.vue";
+import Login from "./components/auth/login.vue";
+import NotFound from "./components/layouts/notFound.vue";
 import Dashboard from "./components/dashboard/index.vue";
 import Users from "./components/users/index.vue";
 import Customers from "./components/customers/index.vue";
 import CustomersAddNew from "./components/customers/AddNew.vue";
-
 import Categories from "./components/categories/index.vue";
 import CategoriesCreate from "./components/categories/create.vue";
 
-const routes = [{
-        path: '/admin',
+const authMiddleware = (to, from, next) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        next();
+    } else {
+        next("/login");
+    }
+};
+
+const routes = [
+    {
+        path: "/admin",
         component: adminLayout,
-        children: [{
+        beforeEnter: authMiddleware,
+        children: [
+            {
                 path: "/dashboard",
                 component: Dashboard,
-                name: 'dashboard'
+                name: "dashboard",
             },
             {
                 path: "/users",
@@ -32,33 +37,35 @@ const routes = [{
             {
                 path: "/customers",
                 component: Customers,
-                name: 'customers.index'
+                name: "customers.index",
             },
             {
                 path: "/customers/create",
                 component: CustomersAddNew,
-                name: 'customers.create'
+                name: "customers.create",
             },
             {
                 path: "/categories",
                 component: Categories,
-                name: 'categories.index'
+                name: "categories.index",
             },
             {
                 path: "/categories/create",
                 component: CategoriesCreate,
-                name: 'categories.create'
+                name: "categories.create",
             },
-        ]
+        ],
     },
     {
-        path: '/login',
+        path: "/login",
         component: authLayout,
-        children: [{
-            path: '',
-            component: Login,
-            name: 'login'
-        }]
+        children: [
+            {
+                path: "",
+                component: Login,
+                name: "login",
+            },
+        ],
     },
     {
         path: "/:pathMatch(.*)*",
@@ -69,7 +76,6 @@ const routes = [{
 
 const router = createRouter({
     history: createWebHistory(),
-    mode: 'history',
     routes,
 });
 
